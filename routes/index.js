3f1162ad81da2741d703cb83d9c8c68a728ae2b7
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-11-11 22:11:14 -0300

    mt-b1-16

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+let router =require('express').Router()
+let user = require('./user.js')
+router.use('/user',user)
+module.exports = router
---
+++ routes/user.js
@@ -0,0 +1,4 @@
+let router=require('express').Router()
+let {create}=require('../controllers/user')
+router.post('/',create)
+module.exports = router;
