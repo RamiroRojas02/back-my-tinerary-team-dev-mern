316ddefb7918db49088bde8dfd1a0c95b817881e
RamiroRojas02 <ramirojasarg@gmail.com>
2022-11-14 13:41:26 -0300

    controlador update/patch shows

--- controllers/show.js
+++ controllers/show.js
@@ -16,6 +16,29 @@ const controller = {
                 message:error.message
             })
         }
+    },
+    update: async(req,res)=>{
+        let {id} = req.params
+        try {
+            let show = await Show.findOneAndUpdate({_id: id},req.body,{new :true})
+            if (show) {
+                res.status(200).json({
+                    id : show._id,
+                    success: true,
+                    message: "Show modified successfully"
+                })
+            }else{
+                res.status(404).json({
+                    success:false,
+                    message: `Show with id : ${id}, doesn't exist`
+                })
+            }
+        } catch (error) {
+            res.status(400).json({
+                success:false,
+                message:error.message
+            })
+        }
     }
 }
 module.exports= controller
