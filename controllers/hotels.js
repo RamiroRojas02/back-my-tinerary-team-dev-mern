d654b9a71ace568ebf6fb41e0b2a30d0384b1845
RamiroRojas02 <ramirojasarg@gmail.com>
2022-11-14 12:28:13 -0300

    defino controlador update/patch para hotels

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -1,9 +1,10 @@
+const Hotel = require('../models/Hotel')
 const hotel = require('../models/Hotel')
 
 
 const controller = {
     create:async(req, res)=>{
-        console.log(req)
+
         try {
             let newHotel = hotel.create(req.body)
             res.status(200).json({
@@ -20,6 +21,30 @@ const controller = {
             
         }
     },
+    update: async(req,res)=>{
+        let { id } = req.params
+        try {
+            let hotel = await Hotel.findOneAndUpdate({_id: id}, req.body, { new : true})
+
+            if (hotel) {
+                res.status(200).json({
+                    success: true,
+                    message: "hotel modified successfully"
+                })
+            }else{
+                res.status(404).json({
+                    success: false,
+                    message: `hotel with id : ${id}, doesn't exist`
+                })
+            }
+        } catch (error) {
+            res.status(404).json({
+                success: false,
+                message : error.message
+
+            })
+        }
+    }
 
 
 }
