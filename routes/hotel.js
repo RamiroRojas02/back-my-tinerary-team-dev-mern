1fae5e71f90a018e81999ce75ce9bd4e28f682fe
RamiroRojas02 <ramirojasarg@gmail.com>
2022-11-14 12:56:35 -0300

    controlador delete hotel

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,6 +1,8 @@
 let router=require('express').Router()
-let {create, update}=require('../controllers/hotels')
+let {create, update, destroy}=require('../controllers/hotels')
 router.post('/',create)
 router.patch('/:id',update)
+router.delete('/:id',destroy)
+
 
 module.exports = router;
