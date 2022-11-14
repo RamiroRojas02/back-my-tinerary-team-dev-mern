0e96ca8dbedbb4a981f0854c12967d73b07f57ca
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-11-14 06:01:46 -0300

    MT-B1-29

--- controllers/city.js
+++ controllers/city.js
@@ -25,6 +25,29 @@ const controller = {
                 message:err.message
             })
         }
+    },
+    destroy:async (req,res)=>{
+        let {id}=req.params
+        try{
+            let deletes= await City.findOneAndDelete({_id:id})
+            if(deletes){
+                res.status(200).json({
+                    id:deletes._id,
+                    success:true,
+                    message:"The city was deleted"
+                })
+            }else{
+                res.status(404).json({
+                    success:false,
+                    message:"Can't found the city"
+                })
+            }
+        }catch(err){
+            res.status(400).json({
+                success:false,
+                message:err.message
+            })
+        }
     }
 }
 module.exports=controller
