2e7112cb836ae3650576e026c471411891db0b53
RamiroRojas02 <ramirojasarg@gmail.com>
2022-11-23 07:29:46 -0300

    MT-02-42

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,13 +1,15 @@
 let router=require('express').Router()
 let validator = require('../middlewares/validator')
 let schema = require('../config/schemas/hotels')
+let schemaEditHotel = require('../config/schemas/hotelEdit')
+
 let {create, update, destroy, read,one}=require('../controllers/hotels')
 router.route('/')
     .get(read)
     .post(validator(schema),create)
 
 router.route('/:id')
-    .patch(update)
+    .patch(validator(schemaEditHotel),update)
     .delete(destroy)
     .get(one)
 
