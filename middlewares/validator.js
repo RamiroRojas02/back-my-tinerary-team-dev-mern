6d95cefc660335c659fe8fce02f8a2ff6d45e27f
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-11-23 05:41:15 -0300

    Test,Alerts & Redireccionamiento

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,8 +1,7 @@
 const validator = (schema) => [
     (req,res,next) =>{
 
-        const data = schema.validate(req.body, {abortEarly : false})
-        
+        const data = schema.validate(req.body, {abortEarly : false})//abortEarly en false valida todos los campos juntos y me devuelve un array con todos los errores de validacion, si fuera en true , valida individualmente(significa que salta la alerta uno en uno , pero es peor experiencia para el usuario)
         if (data.error) {
             return res.status(200).json({
                 success: false,
@@ -14,3 +13,5 @@ const validator = (schema) => [
 ]
 
 module.exports = validator
+//el metodo validate , devuelve un objeto con propiedades, en el cual una de ellas tiene un array con todos los errores el objeto se llama data, y todos los errores estan en data.error.details
+//un validator es un middleware creado para poder ingresar entre medio de la carga de los datos y asi poder alertar si es que alguna validacion es erronea , para que no permita pasar al metodo de la "creacion"
--- routes/city.js
+++ routes/city.js
@@ -1,8 +1,14 @@
 let router=require('express').Router()
+const validator = require('../middlewares/validator')
+const schema= require('../config/schemas/cities')
 let {create,update,destroy,read, readDetails}=require('../controllers/city')
-router.get('/',read)
-router.get('/:id',readDetails)
-router.post('/',create)
-router.put('/:id',update)
-router.delete('/:id',destroy)
+router.route('/')
+.get(read)
+.post(validator(schema),create)
+
+router.route('/:id')
+.get(readDetails)
+.put(update)
+.delete(destroy)
+
 module.exports = router;
