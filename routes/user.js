d79326eb4c3d4359b3c9044eee01b83dddc70243
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-11-27 22:14:16 -0300

    correccion de rutas

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,9 @@ const mustSignIn = require('../middlewares/mustSignIn')
 const validator = require('../middlewares/validator')
 const schemaSignIn = require('../config/schemas/userSignIn')
 const schema= require('../config/schemas/users')
-const accountExistsSignUp = require('../middlewares/accountExistsSignUp')
+const {accountExists1} = require('../middlewares/accountExistsSignUp')
 //primero valido con joi, luego verifico si la cuenta existe y si todo va bien creo el usuario
-router.post('/SignUp',validator(schema),accountExistsSignUp,register)
+router.post('/SignUp',validator(schema),accountExists1,register)
 router.get('/verify/:code',verify)
 
 router.post('/signin',validator(schemaSignIn),accountExists,accountHasBeenVerified,signin)
